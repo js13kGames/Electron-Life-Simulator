da5a574fb3aec4f541fbd85a12332a6fa9e08790
lespin <jeanpitouconsulting@free.fr>
2020-08-31 13:08:34 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -81,249 +81,9 @@ const texts = Texts()
 //     tc.needsUpdate = true
 //     tp.needsUpdate = true
 // }
-
-// function initLevel()
-// {
-//     STATE = 'S00'
-//     const { choices } = State.Choices
-    
-//     player.mesh.position.x = 0
-//     player.mesh.position.y = choices[0].ys[ choices[0].good ] + 0.5
-//     // player.speed = new THREE.Vector2(0,0)
-//     player.life = 1000
-
-//     updateCamera( player.mesh.position )
-//     camera.position.z = 100
-// }
-
-
-// var animate = function (T) {
-//     //    console.log(STATE)
-//     requestAnimationFrame( animate )
-//     const dt = ( lastT === undefined )?0:(T-lastT)
-//     lastT = T
-//     stats.begin()
-//     if ( STATE === 'S00' ){
-//         textPanels.nextlevel.mesh.visible = false
-//         /*State.Choices.mesh.visible = false
-//           setTimeout( () => {
-//           nextLevel()
-//           STATE = 'S0'
-//           }, 10000 )*/
-
-        
-//         const dirs = State.Choices.directions
-//         const choicesMesh = State.Choices.mesh
-//         const fs = [
-//             () => {
-//                 choicesMesh.visible = true
-//                 player.mesh.visible = false
-
-//                 textPanels.go.mesh.visible = true
-//                 textPanels.go.mesh.position.x = camera.position.x 
-//                 textPanels.go.mesh.position.y = camera.position.y + 4
-//             },
-//             () => {
-//                 //textPanels.go.mesh.visible = false
-//             }
-//         ]
-        
-//         for ( let ii = 0 ; ii < dirs.length ; ii++ ){
-//             const i = ii
-//             fs.push( () => {
-//                 if ( i > 0 ){
-//                     const ldir = dirs[ i - 1 ]
-//                     const tp = textPanels['c'+ldir]
-//                     tp.mesh.visible = false
-//                 }
-//             })
-//             fs.push( () => {
-//                 const dir = dirs[ i ]
-//                 const tp = textPanels['c'+dir]
-//                 tp.mesh.visible = true
-//                 tp.mesh.position.x = camera.position.x + i * 0.1
-//                 tp.mesh.position.y = camera.position.y
-//             })
-//         }
-//         fs.push( () => {} )
-//         fs.push( () => {
-            
-//             for ( let i = 0 ; i < dirs.length ; i++ ){
-//                 const tp = textPanels['c'+dirs[i]]
-//                 tp.mesh.visible = false
-//             }
-//             STATE = 'S0'
-//         })
-//         STATE = 'S00S0'
-//         fs.forEach( (f,i) => {
-//             setTimeout( f, (1+i) * 600 )
-//         })
-//     } else if ( STATE === 'S00S0' ){
-//         textPanels.go.animate(0.25)
-//     } else if ( STATE === 'S0' ){
-//         const choicesMesh = State.Choices.mesh
-//         choicesMesh.visible = true
-//         player.mesh.visible = true
-//         textPanels.go.animate(0.25)
-
-//         //textPanels.go.mesh.position.x = camera.position.x 
-//         //textPanels.go.mesh.position.y = camera.position.y
-//         //textPanels.go.mesh.visible = true
-//         setTimeout( ()=>{
-//             textPanels.go.mesh.visible = false
-//         } ,  1000 )
-//         setTimeout( ()=>{
-//             STATE = 'S1'
-//         } ,  2000 )
-        
-//     } else if ( STATE === 'S2' ){
-//         textPanels.failed.mesh.visible = true
-//         textPanels.failed.animate(0.25)
-//         textPanels.failed.mesh.position.x = camera.position.x 
-//         textPanels.failed.mesh.position.y = camera.position.y - 4
-//     }else if ( STATE === 'S3' ){
-//         player.mesh.position.x +=  dt / 20
-//         camera.position.x +=  dt / 20
-//         textPanels.success.animate(0.25)
-//         textPanels.success.mesh.visible = true
-//         textPanels.success.mesh.position.x = camera.position.x 
-//         textPanels.success.mesh.position.y = camera.position.y - 4
-//         //keyboardController.axesCtrlState[1] = [0,0]
-//         updateTrail(camera.position, false, STATE)
-//     }  else if ( STATE === 'S4' ){
-//         player.mesh.visible = false
-//         textPanels.success.mesh.visible = false
-//         textPanels.nextlevel.animate(0.25)
-//         textPanels.nextlevel.mesh.visible = true
-//         textPanels.nextlevel.mesh.position.x = camera.position.x 
-//         textPanels.nextlevel.mesh.position.y = camera.position.y - 4
-//         updateTrail(camera.position, false, STATE)
-//     } else if ( STATE === 'S1' ){
-
-//         keyboardController.axesCtrlState[0] = [0,1]
-//         const [[l,r],[d,u],[o,p]] = keyboardController.axesCtrlState    
-//         const dx = - 1 * l + r,
-//               dy = -1 * d + u,
-//               dz = -1 * o + p
-        
-//         const speed01 = 1,
-//               minspeed = 150,
-//               maxspeed = 40,
-//               ff = (1-speed01) * minspeed + speed01 * maxspeed,
-//               pmp = player.mesh.position
-        
-        
-//         const { ij2idx, map } = State.Choices
-        
-//         function posCollide( pos, cells ){
-//             const i = Math.round(pos.x-0.5),
-//                   j = Math.round(pos.y-0.5),
-//                   c = map[ ij2idx( i,j ) ]
-//             const collides = cells.includes( c )
-//             return collides
-//         }
-        
-//         const nextPosition = pmp.clone()
-//         let hasCollision = false        
-//         for ( let i = 3 ; i >= 1 ; i-- ){
-//             if ( i&1 ) nextPosition.x += dx * dt / ff
-//             if ( i&2 ) nextPosition.y += dy * dt / ff
-//             const collide = posCollide( nextPosition, ['*'] )
-//             if ( !collide ) break;
-//             hasCollision = true
-//             nextPosition.copy( pmp )
-//         }
-//         if ( posCollide( nextPosition, 'G' ) ){
-//             //if ( STATE = 'S3' ){
-//             setTimeout( () => {
-//                 STATE = 'S4'
-//                 setTimeout( () => {
-//                     nextLevel()
-//                 },4000)                
-//             },2000)
-//             //}
-//         }
-//         //hasCollision = false
-//         /*
-//           nextPosition.x += dx * dt / ff
-//           nextPosition.y += dy * dt / ff
-//         */
-        
-        
-//         updateTrail(pmp, hasCollision)
-        
-//         if ( hasCollision ){
-//             let damage
-//             if (nextPosition.equals(pmp) ){
-//                 damage = dt * 4
-//             } else {
-//                 damage = dt 
-//             }
-//             player.life -= damage
-//             if ( player.life < 0 ){
-//                 STATE = 'S2'
-//                 player.mesh.material.color.setRGB(1,0,0)
-//                 setTimeout( () => {
-//                     textPanels.failed.mesh.visible = false
-//                     initLevel()
-//                 },3000)
-//             }
-//         }
-//         player.mesh.position.copy( nextPosition )
-        
-//         {
-//             const { ij2idx, map } = State.Choices
-//             const i = Math.round(nextPosition.x-0.5),
-//                   j = Math.round(nextPosition.y-0.5),
-//                   c = map[ ij2idx( i,j ) ]
-//             const col = Cols[c] || [1,0,0]
-            
-//             player.mesh.material.color.setRGB( ...col )
-//             if ( hasCollision ){
-//                 player.mesh.material.color.setRGB( (1 - player.life/1000),0,0 )
-//             } else {
-//                 player.mesh.material.color.setRGB( ...col )
-//             }
-            
-//         }
-//         camera.position.z += dz * dt / ff
-//     }
-
-//     updateCamera( player.mesh.position )
-    
-//     keyboardController.resetKeyStrokes()
-//     /*
-//       var elapsedMilliseconds = Date.now() - startTime;
-//       var elapsedSeconds = elapsedMilliseconds / 1000.0;
-//     */
-//     /*material.uniforms.time.value = 60. * elapsedSeconds;    
-//       material.uniforms.resolution.value.x = renderer.domElement.width;
-//       material.uniforms.resolution.value.y = renderer.domElement.height;
-//     */
-//     //renderer.domElement.setAttribute('class','three')
-//     renderer.render( scene, camera );
-//     stats.end()
-// };
-
-// //animate();
-
-
-
-
-// /*
-//   const levels = terrainData.trim().split('==').map( level => {
-
-//   const [name,...data] = level.trim().split("\n")
-//   mkLevel( name, data )
-//   //console.log('name','+'+name+'+')
-//   //console.log('data','+'+data+'+')
-//   })
-
-// */
-
-// scene.add( trail.mesh );
-// 300 / 600 rect / ms
 function Display(){
+    
+    // 300 / 600 rect / ms
     
     const canvas = document.createElement('canvas')
     canvas.width =  targetSize.width 
@@ -334,13 +94,12 @@ function Display(){
     const context = canvas.getContext('2d')
     document.body.appendChild( canvas )
     
-    const choices = mkChoices() 
-    const { startPosition, map, width, height, idx2j, idx2i, ij2idx,
-            directions, outij } = choices
-
+    //const choices = mkChoices() 
     const nominalScale =16
+    
     const camera = {
-        center : { ...startPosition },
+        //center : { ...startPosition },
+        center : { x:0,y:10},
         scale : nominalScale,
         nominalScale
     }
@@ -349,9 +108,10 @@ function Display(){
         context.fillStyle = 'rgba(0,127,200,1)'
         context.fillRect(0,0,canvas.width,canvas.height)
     }
-    function draw( { center, scale }, level ){
-        const width = level.width,
-              height = level.height,        
+    function draw( { center, scale }, level, player ){
+    
+        const/* width = level.width,
+              height = level.height,        */
               canvas = context.canvas,
               cwidth = canvas.width,
               cheight = canvas.height,
@@ -359,6 +119,8 @@ function Display(){
               hcHeight = cheight / 2
         
         function drawMap(){
+            const { map, width, height,
+                    ij2idx, outij } = level
             const  cpx = center.x + 0.5,
                   cpy = center.y + 0.5,
                   visiblemap = {
@@ -396,36 +158,53 @@ function Display(){
                 )
             }
         }
+        function box2screen( x,y,w,h, target ){
+            target[0] = (x - w/2 - center.x) * scale + hcWidth
+            target[1] = (y - h/2 - center.y) * scale + hcHeight
+            target[2] = w * scale
+            target[3] = h * scale
+            return target
+        }
         function drawPlayer( context, { center, scale } )
         {
-            const position = { ...startPosition }
+            const sb = []
+            //const position = { ...startPosition }
             const dim = 1
-            position.x = 0
-            const x = (position.x - dim/2 - center.x) * scale + hcWidth
-            const y = (position.y - dim/2 - center.y) * scale + hcHeight
-            
+            //position.x = 0
             context.fillStyle = cssrgba(1,0,0)
+            /*
+            const x = (position.x - dim/2 - center.x) * scale + hcWidth
+            const y = (position.y - dim/2 - center.y) * scale + hcHeight            
             context.fillRect(x,y, dim*scale,dim*scale)
+            */
+            context.fillRect( ...box2screen( player.position.x,
+                                             player.position.y,
+                                             dim, dim, sb ) )
         }
         function drawParticle( context, { center, scale }, i )
         {
-            const position = { ...startPosition }
+            const sb = []
+            const position = cloneV2(player.position)//{ ...startPosition }
             const dim = 0.5
-            
             const d = Math.cos( i/10+ Date.now() / 1000 ) * 10
             position.x += Math.cos( i+Date.now() / 1000 ) * d
             position.y += Math.sin( i+Date.now() / 1000 ) * d
+
+            context.fillStyle = cssrgba(Math.random(),Math.cos( i/10),1)
             
+            /*
             const x = (position.x - dim/2 - center.x) * scale + hcWidth
             const y = (position.y - dim/2 - center.y) * scale + hcHeight            
-            context.fillStyle = cssrgba(Math.random(),Math.cos( i/10),1)
             context.fillRect(x,y, dim*scale,dim*scale)
+            */
+            context.fillRect( ...box2screen( position.x, position.y, dim, dim, sb ) )
         }
 
         const t1 = Date.now()
+        if (level)
         drawMap()
         const t2 = Date.now()
-        drawPlayer( context, camera,)
+        drawPlayer( context, camera)
         const t3 = Date.now()        
         const NB_PARTICLES = 100
         for ( let i = 0 ; i < NB_PARTICLES ; i++ ){
@@ -444,7 +223,7 @@ function Display(){
         //console.log(e2,e3,e4,e5,e)
         return e
     }
-    return { clear, draw, camera, choices }
+    return { clear, draw, camera }
 }
 
 
@@ -457,22 +236,25 @@ function cssrgba( r01,g01,b01,a=1){
           b = Math.floor( 256 * b01 )
     return `rgba(${r},${g},${b},${a})`
 }
-function movePlayer( player, level, dx, dy, dt, ff){
-    const { ij2idx, map } = level,
-          pmp = player.position
-    const nextPosition = cloneV2(pmp)
-    let hasCollision = false        
-    function posCollide( pos, cells ){
-        const i = Math.round(pos.x-0.5),
-              j = Math.round(pos.y-0.5),
-              c = map[ ij2idx( i,j ) ]
-        const collides = cells.includes( c )
+function posCollide( level, pos, cells ){
+    const { ij2idx, map, outij } = level,
+          i = Math.round(pos.x-0.5),
+          j = Math.round(pos.y-0.5)
+    if ( !outij(i,j) ) {
+        const c = map[ ij2idx( i,j ) ],
+              collides = cells.includes( c )
         return collides
     }
+}
+
+function movePlayer( player, level, dx, dy, dt, ff){
+    const pmp = player.position,
+          nextPosition = cloneV2(pmp)
+    let hasCollision = false        
     for ( let i = 3 ; i >= 1 ; i-- ){
         if ( i&1 ) nextPosition.x += dx * dt / ff
-        if ( i&2 ) nextPosition.y += dy * dt / ff
-        const collide = posCollide( nextPosition, ['*'] )
+        if ( i&2 ) nextPosition.y += dy * dt / ff 
+        const collide = posCollide( level, nextPosition, ['*'] )
         if ( !collide ) break;
         hasCollision = true
         copyV2(pmp,nextPosition)
@@ -487,7 +269,8 @@ function GameState(){
 
     const state = {
         name : 'I0',
-    }    
+    }
+    const timeouts = [] 
     function update( ...ps ){
         console.log('update',...ps)
         Object.assign( state, ...ps )
@@ -497,6 +280,7 @@ function GameState(){
     const NLEVELS = 3
     const NSUBLEVELS = 2
     const NLIVES = 2
+    const NOMINAL_ENERGY = 1000
     const automata = {
         // boot intro and warm welcoming message   
         I0 : {
@@ -526,17 +310,28 @@ function GameState(){
         // level / sublevel presentation
         S1 : {
             'next' : d => update({
-                name : 'S2',
+                name : 'S2',                
             })
         },
         // ready / set / go
         S2 : {
             'next' : d => update({
+                energy : NOMINAL_ENERGY,
                 name : 'S3',
             })
         },
         // runnning
         S3 : {
+            'damage' : d => {
+                const energy = state.energy - d
+                if ( (energy - d)  > 0 ){
+                    update({
+                        energy : state.energy - d
+                    })
+                } else {
+                    event('sublevel-lose')
+                }
+            },
             'sublevel-win' : d => update({
                 name : 'W1'
             }),
@@ -602,7 +397,7 @@ function GameState(){
             })
         }
     }
-    const timeouts = []
+ 
     function timeout( f, delay ){
         const start = Date.now(),
               end = start + delay,
@@ -618,7 +413,6 @@ function GameState(){
         if ( first ) {
             const now = Date.now()
             if ( now >= first.end ){
-                console.log('here',first)
                 first.f()
             }
         }
@@ -654,33 +448,68 @@ const gameState = GameState()
 const display = Display()
 const player = Player()
 const step = dt=>{
-    stats.begin()
-    // grab input
-    const [[l,r],[d,u],[o,p]] = keyboardController.axesCtrlState
+//    console.log(gameState.state.name)
     const camera = display.camera
-    const choices = display.choices
-    // move
-    const speed01 = 1,
-          minspeed = 150,
-          maxspeed = 40,
-          ff = (1-speed01) * minspeed + speed01 * maxspeed
-    const dx = ( -1*l + r ) 
-    const dy = ( -1*u + d ) 
-    const { nextPosition, hasCollision } =  movePlayer( player, choices, dx, dy, dt, ff )
-    if ( hasCollision ){
-        copyV2( camera.center, nextPosition )
+    const choices = gameState.state.choices
 
+    // check timeouts
+    gameState.checkTimeouts()
+    // grab input
+    stats.begin()
+    if ( ['I0','I1','G0','G1','S1','S2',
+          'L1','L2'].includes( gameState.state.name ) ){
+        if ( keyboardController.anyKeyStroke.length ){
+            console.log('to!')
+            gameState.event('next')
+        }
     }
-    copyV2( nextPosition, camera.center )
 
+    const [[l,r],[d,u],[o,p]] = keyboardController.axesCtrlState
+    keyboardController.resetKeyStrokes()
+    if ( ['S2'].includes( gameState.state.name ) ){
+        copyV2(choices.startPosition, player.position)        
+    }
+    if ( ['S3'].includes( gameState.state.name ) ){
+//        console.log('gameState',gameState)
+        // player move + collide
+        const speed01 = 1,
+              minspeed = 150,
+              maxspeed = 40,
+              ff = (1-speed01) * minspeed + speed01 * maxspeed
+        const dx = ( -1*l + r ) 
+        const dy = ( -1*u + d )
+        const { nextPosition, hasCollision } =  movePlayer( player, choices, dx, dy, dt, ff )
+        copyV2(nextPosition,player.position)
+        copyV2(player.position,display.camera.center)
+        
+        // update particles
+        //if ( hasCollision ){
+        //}
+        
+        // check victory
+        if ( posCollide( choices, nextPosition, 'G' ) ){
+            gameState.event('sublevel-win')
+        }
+        
+        // check damage
+        if ( hasCollision ){
+            //copyV2( camera.center, nextPosition )
+            gameState.event('damage',10)
+        }
+    }
+    
+    /*
+      
+    copyV2( nextPosition, camera.center )
+    */
     // display
     display.clear( )                
-    camera.center.x += ( -1*l + r ) * dt / 100
-    camera.center.y += ( -1*u + d ) * dt / 100
+   /* camera.center.x += ( -1*l + r ) * dt / 1000
+    camera.center.y += ( -1*u + d ) * dt / 1000*/
     camera.scale *= ( 1 + ( -1*o + p ) / 10 ) 
     camera.scale = clamp( camera.scale, 4,32) // 4 wide zoom, 32 closeup
     
-    const elapsed1 = display.draw( camera, choices )
+    const elapsed1 = display.draw( camera, choices, player )
     
     stats.end()
 
--- src/keyboardControllers.js
+++ src/keyboardControllers.js
@@ -1,19 +1,25 @@
 export function KeyboardControllers(){
     const keyMap = ['ArrowLeft','ArrowDown','ArrowRight','ArrowUp'] // wasd
     const keyStrokes = []
+    const anyKeyStroke = []
     function resetKeyStrokes(){
         keyStrokes.length = 0
+        anyKeyStroke.length = 0
     }
     const keyStrokesHandler = type => ({key,repeat,timeStamp})=> {
+        //console.log(key,keyStrokes)
         if ( !repeat ){
+            if ( type )
+                anyKeyStroke[0]=1
+            
             const idx = keyMap.findIndex(x=>x==key)
             if ( idx >= 0 ) keyStrokes.push([type,idx,timeStamp])
         }   
     }
-    /*
-      window.addEventListener('keydown', keyStrokesHandler(1))
-      window.addEventListener('keyup', keyStrokesHandler(0))
-    */
+    
+    window.addEventListener('keydown', keyStrokesHandler(1))
+    window.addEventListener('keyup', keyStrokesHandler(0))
+    
     
     const axesKeyMap = [['ArrowLeft','ArrowRight'],['ArrowDown','ArrowUp'],['+','-']]
     const axesCtrlState = [[0,0],[0,0],[0,0]]
@@ -32,5 +38,5 @@ export function KeyboardControllers(){
     }
     window.addEventListener('keydown', axesCtrlHandler(1))
     window.addEventListener('keyup', axesCtrlHandler(0))
-    return { keyStrokes, resetKeyStrokes, axesCtrlState }
+    return { keyStrokes, anyKeyStroke, resetKeyStrokes, axesCtrlState }
 }
