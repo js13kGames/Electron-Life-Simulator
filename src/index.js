5cb9a6c2a772a5f627ff88e157b4ccd376724ee7
lespin <jeanpitouconsulting@free.fr>
2020-09-02 09:40:46 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import { textCanvas } from './textPlane.js'
 import { V2, cloneV2, copyV2, subV2, addV2, multScalar, divScalar, clampV2, ceilV2, floorV2, lerpV2} from './v2.js'
 import { Roller } from './roller.js'
 import { canvasStyle, bodyStyle } from './css.js'
+import { FeedbackBuffer } from './feedbackBuffer.js'
 const { zzfx } = require('./zz.js')
 
 document.body.style = 'background:#111;';
@@ -106,102 +107,8 @@ function Display(){
     canvas.style = canvasStyle//'position: absolute ; top : 200px ; left : 0px;'   
     const context = canvas.getContext('2d')
 
-    function BlurBuffer(){
 
-        const secondCanvas = document.createElement('canvas')
-        secondCanvas.width =  targetSize.width 
-        secondCanvas.height = targetSize.height
-        
-        const secondContext = secondCanvas.getContext('2d')
-        document.body.appendChild( secondCanvas )
-
-        function pixelFilter1(srcData,dstData,i){
-            dstData[ i ] = srcData[ i ]
-            dstData[ i + 1 ] = srcData[ i + 1]  / 2
-            dstData[ i + 2 ] = srcData[ i + 1]
-            if ( Math.random() > 0.8 ){
-                dstData[ i + 3 ] = Math.max(0,srcData[ i + 1] - 1)
-            } else {
-                dstData[ i + 3 ] = srcData[ i + 1]
-            }
-        }
-        function pixelFilter2(srcData,dstData,i){
-            dstData[ i ] = srcData[ i + 1 ]
-            dstData[ i + 1 ] = Math.floor(srcData[ i + 2]  + 10,255)
-            dstData[ i + 2 ] = srcData[ i + 3]
-            dstData[ i + 3 ] = Math.max(0,srcData[ i + 0] - 10)
-        }
-        function pixelFilter3(sub=40,mul=0.9){
-            return function(srcData,dstData,i){
-                dstData[ i ] = srcData[ i + 0 ]
-                dstData[ i + 1 ] = srcData[ i + 1 ]
-                dstData[ i + 2 ] = srcData[ i + 2 ]
-                dstData[ i + 3 ] = Math.max(0,srcData[ i + 3 ]*mul - sub )
-            }
-        }
-        function pixelFilter4(srcData,dstData,i){
-            const sub = 20
-            dstData[ i ] = srcData[ i + 1 ]
-            dstData[ i + 1 ] = srcData[ i + 2 ]
-            dstData[ i + 2 ] = srcData[ i + 4 ]
-            dstData[ i + 3 ] = Math.max(0,srcData[ i + 3 ] - sub )
-        }
-        function paste1(){
-            const xoff = Math.cos( Date.now() / 100 ) * 6
-            const yoff = Math.sin( Date.now() / 100 ) * 6
-            if ( used ){
-                context.drawImage(secondCanvas,xoff,yoff)                
-            }
-        }        
-        function paste2(){
-            if ( used ){
-                context.drawImage(secondCanvas,0,0)
-            }
-        }        
-        function paste3(){
-            const xoff = -1
-            if ( used ){
-                context.drawImage(secondCanvas,xoff,0)                
-            }
-        }        
-        const modes = {
-            'none' : [ pixelFilter3(0,0), paste2 ],
-            'one' : [ pixelFilter1, paste1 ],
-            'two' : [ pixelFilter2, paste2 ],
-            'three' : [ pixelFilter3(40,1), paste2 ],
-            'four' : [ pixelFilter4, paste3 ],
-        }
-        function getPixelFilter( n ){ return modes[ n ][ 0 ] }
-        function getPaste( n ){ return modes[ n ][ 1 ]  }
-        const modeName = 'one'
-        let used = false
-        function copy(){
-            const pixelFilter = getPixelFilter( modeName ),
-                  src = context.getImageData(0,0,canvas.width,canvas.height),
-                  dst = secondContext.getImageData(0,0,canvas.width,canvas.height),
-                  srcData = src.data,
-                  dstData = dst.data
-            for ( let i = 0 ; i < srcData.length ; i+= 4 ){
-                //pixelFilter1(srcData,dstData,i)
-                //pixelFilter2(srcData,dstData,i)
-                pixelFilter(srcData,dstData,i)
-            }
-            secondContext.putImageData( dst,0,0)
-            used = true
-        }
-        function paste(){
-            if ( used ){
-                const paste = getPaste( modeName )
-                paste()
-            }
-        }
-        function reset(){
-            used = false
-        }
-        return { copy, paste, reset }
-    }
-
-    const blurBuffer = BlurBuffer( )
+    const feedbackBuffer = FeedbackBuffer( context )
     
     document.body.appendChild( canvas )
     
@@ -219,7 +126,7 @@ function Display(){
           hcHeight = canvas.height / 2
     
     function newframe(  ){
-        blurBuffer.copy()
+        feedbackBuffer.copy()
         //context.fillStyle = 'rgba(0,127,200,1)'
         context.fillStyle = 'rgba(0,0,0,1)'
         context.fillRect(0,0,canvas.width,canvas.height)
@@ -404,7 +311,7 @@ function Display(){
         if ( timeoutBar.visible ){
             drawTimeoutBar()
         }
-        blurBuffer.paste()
+        feedbackBuffer.paste()
         const t6 = Date.now()
         if ( lifeBar.visible ){
             drawLifeBar()
