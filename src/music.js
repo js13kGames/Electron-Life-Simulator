7a6aafde04c20100814c41bd01fc6255a32a9c57
lespin <jeanpitouconsulting@free.fr>
2020-09-03 10:17:05 +0200

    ...

--- src/music.js
+++ src/music.js
@@ -1,5 +1,6 @@
-function ms(ms){ return ms / 1000 }
+// general
 
+function ms(ms){ return ms / 1000 }
 function ftok( f ){
     return 69 + 12 * Math.log2( f / 440 )
 }
@@ -10,67 +11,6 @@ function itrvstot( itrvs, t0 ){
     return itrvs.reduce( (r,x) => {
         return [ ...r, r[r.length-1] + x ]
     },[t0])
-}
-function adsr( ap, { values, durations }, t ){
-    //     adsr              gain
-    //      o                0
-    //     o ooo             1
-    // 0_ o     o  _0
-    //
-    //      ad sr
-    //    0 12 3 4
-    const times = itrvstot( durations, t )
-    ap.setValueAtTime(0, times[0] )        
-    ap.linearRampToValueAtTime( values[0], times[1] )
-    ap.linearRampToValueAtTime( values[1], times[2] )
-    ap.linearRampToValueAtTime( values[1], times[3] )
-    ap.linearRampToValueAtTime( 0, times[4] )
-    return times[4]
-}
-function asr( ap, { values, durations }, t ){
-    //   a    s   r          gain
-    //       oooo            1
-    //     oo    o           0
-    // 1_ o       o _ 1       1
-    //
-    //    0  1  2 3
-    
-    const times = itrvstot( durations, t )
-    if ( times[0] === times[1] ){
-        ap.setValueAtTime( values[0], times[1] )
-    } else { 
-        ap.setValueAtTime( 1, times[0] )
-        ap.linearRampToValueAtTime( values[0], times[1] )
-    }
-    ap.linearRampToValueAtTime( values[1], times[2])
-    if ( times[2] === times[3] ){
-    } else {
-        ap.linearRampToValueAtTime( 1, times[3])
-    }
-    
-    //ap.linearRampToValueAtTime( 20, t+0.5)
-    //ap.linearRampToValueAtTime( 22, t+2)
-    //ap.linearRampToValueAtTime( 1, t+2.3)
-    
-    return times[3]            
-}
-function DelayChain( ac, ds  ){
-    const nodes = []
-    for ( let i = 0 ; i < ds.length ; i++ ){
-        const gn = ac.createGain(),
-              dn = ac.createDelay()
-        gn.gain.value = ds[ i ][1]
-        dn.delayTime.value = ds[ i ][0]
-        gn.connect( dn )
-        if ( i > 0 ){
-            nodes[ node.length - 1 ].connect( gn )
-        }
-        nodes.push( gn, dn )
-    }
-    const input = nodes[0],
-          output = nodes[nodes.length-1]
-    output.connect( input )
-    return { input, output  }
 }
 function FftFreqs(size,f0){
     // relation between frequency, midi key and fft bins
@@ -95,6 +35,63 @@ function FftFreqs(size,f0){
         return _nbcache[ k ]
     }
     return { size, f0, bins, nearestBins }
+}
+
+// enveloppes
+function adsr( ap, { values, durations }, t ){
+    //     adsr              gain
+    //      o                0
+    //     o ooo             1
+    // 0_ o     o  _0
+    //
+    //      ad sr
+    //    0 12 3 4
+    const times = itrvstot( durations, t )
+    ap.setValueAtTime(0, times[0] )        
+    ap.linearRampToValueAtTime( values[0], times[1] )
+    ap.linearRampToValueAtTime( values[1], times[2] )
+    ap.linearRampToValueAtTime( values[1], times[3] )
+    ap.linearRampToValueAtTime( 0, times[4] )
+    return times[4]
+}
+function asr( ap, { values, durations }, t ){
+    //   a    s   r          gain
+    //       oooo            1
+    //     oo    o           0
+    // 1_ o       o _ 1       1
+    //
+    //    0  1  2 3    
+    const times = itrvstot( durations, t )
+    if ( times[0] === times[1] ){
+        ap.setValueAtTime( values[0], times[1] )
+    } else { 
+        ap.setValueAtTime( 1, times[0] )
+        ap.linearRampToValueAtTime( values[0], times[1] )
+    }
+    ap.linearRampToValueAtTime( values[1], times[2])
+    if ( times[2] === times[3] ){
+    } else {
+        ap.linearRampToValueAtTime( 1, times[3])
+    }
+    return times[3]            
+}
+function DelayChain( ac, ds  ){
+    const nodes = []
+    for ( let i = 0 ; i < ds.length ; i++ ){
+        const gn = ac.createGain(),
+              dn = ac.createDelay()
+        gn.gain.value = ds[ i ][1]
+        dn.delayTime.value = ds[ i ][0]
+        gn.connect( dn )
+        if ( i > 0 ){
+            nodes[ node.length - 1 ].connect( gn )
+        }
+        nodes.push( gn, dn )
+    }
+    const input = nodes[0],
+          output = nodes[nodes.length-1]
+    output.connect( input )
+    return { input, output  }
 }
 function periodWaveFromKeys( ac, chord, { size, nearestBins } ){
     const real = new Float32Array(size)
@@ -128,11 +125,11 @@ export function play(){
             })
         }
     }
-    let t = 0
 
     const delayChain = DelayChain(ac, [[0.3,0.6]/*,[0.1,0.6]*/] )
     delayChain.output.connect( ac.destination )   
     
+    let t = 0
     chords.forEach( chord => {
         t = playChord( chord, t )
     })
@@ -141,6 +138,7 @@ export function play(){
     function playChord( chord, t ){
         
         const wave = periodWaveFromKeys( ac, chord, fftFreqs )
+        
         const osc = ac.createOscillator();
         osc.frequency.value = fftFreqs.f0
         osc.setPeriodicWave(wave);
@@ -157,27 +155,23 @@ export function play(){
 
         const vibratoEnvelope = {
             values : [ 20, 22 ],
-            durations : [ 0.5, 2-0.5,2.3-2 ]
+            durations : [ 0.5, 1.5, 0.3 ]
         }
-        
-        
         
         // freq osc
         {
             const mod = ac.createOscillator();
             mod.frequency.value = 5
             const modGain = ac.createGain()
-            //modGain.gain.value = 10.0
             const vibEnvEnd = asr( modGain.gain, vibratoEnvelope, t )
             modGain.connect( osc.detune )
             mod.connect( modGain )
             mod.start(t)
         }
-        
+
+        // slight up ramp for frequency
         osc.frequency.setValueAtTime( fftFreqs.f0, t )
         osc.frequency.linearRampToValueAtTime( fftFreqs.f0*1.02, envEnd )
-        
-        
         
         osc.start( t )
         osc.stop( envEnd )
